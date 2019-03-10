e2ea866991650c616b6f2864d27e08b60c089c6a
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-03-10 23:19:54 +0800

    Add favicon

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+
 const index = require("./routes/index");
 const greet = require("./routes/greet");
+
+app.use(express.static("public"));
 
 app.use("/", index);
 app.use("/greet", greet);
