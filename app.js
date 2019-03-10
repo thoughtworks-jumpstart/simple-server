6b32f51e5b1c5944a306a4d86cb2b80990e8437c
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-03-10 22:42:51 +0800

    Add /greet to greet someone by name

--- app.js
+++ app.js
@@ -3,4 +3,12 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello, world!"));
 
+app.get("/greet", (req, res) => {
+  let greeting = "Greet someone by name with /greet?name='Jane Doe'";
+  if (req.query.name != undefined) {
+    greeting = `Hello, ${req.query.name}!`;
+  }
+  res.send(greeting);
+});
+
 module.exports = app;
--- app.test.js
+++ app.test.js
@@ -5,6 +5,21 @@ describe("Server", () => {
   test("GET /", () => {
     return request(app)
       .get("/")
-      .expect(200);
+      .expect(200)
+      .expect("Hello, world!");
+  });
+
+  test("GET /greet", () => {
+    return request(app)
+      .get("/greet")
+      .expect(200)
+      .expect("Greet someone by name with /greet?name='Jane Doe'");
+  });
+
+  test("GET /greet?name=Bob", () => {
+    return request(app)
+      .get("/greet?name=Bob")
+      .expect(200)
+      .expect("Hello, Bob!");
   });
 });
