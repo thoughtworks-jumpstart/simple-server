c701fcd2daf045fd933839e930ec4a95278bb2a5
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-03-10 22:21:37 +0800

    Define port number for server

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const port = 8080;
 
 app.get("/", (req, res) => res.send("Hello, world!"));
 
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const app = require("./app");
+const port = 8080;
 
 app.listen(port, () =>
   console.log(`Server is now running on http://localhost:${port}`)
